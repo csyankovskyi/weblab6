8a2ee03a133fd85ba3ff19c30b81be8e5151c8ea
Bohdan Yankovskyi <asdec344e@gmail.com>
2023-01-11 18:31:45 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,5 @@
+<form action="action.php" method="post">
+ <p>Ваше ім'я: <input type="text" name="name" /></p>
+ <p>Ваш вік: <input type="text" name="age" /></p>
+ <p><input type="submit" /></p>
+</form>
